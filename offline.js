06fb51c1da34359e60f2b90769c956a033df308b
Paxnar <67262211+Paxnar@users.noreply.github.com>
2024-05-23 00:41:49 +0300

    Add files via upload
    what are you expecting to see here honestly

---
+++ offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1716413955,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"gataria-sheet0.png",
+		"sprite-sheet0.png",
+		"sprite2-sheet0.png",
+		"sprite3-sheet0.png",
+		"sprite4-sheet0.png",
+		"sprite5-sheet0.png",
+		"sprite6-sheet0.png",
+		"sprite7-sheet0.png",
+		"sprite8-sheet0.png",
+		"bg-sheet0.png",
+		"Запись (8).m4a",
+		"gataria.ogg",
+		"gataria intro.ogg",
+		"sad gataria.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
